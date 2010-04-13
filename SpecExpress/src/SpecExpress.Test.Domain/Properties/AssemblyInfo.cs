b0436276f7122b9a97930ce8369505b91b093de3
Alan Baker <technoag@gmail.com>
2010-04-13 16:07:40 +0000

    Build 45068

--- SpecExpress/src/SpecExpress.Test.Domain/Properties/AssemblyInfo.cs
+++ SpecExpress/src/SpecExpress.Test.Domain/Properties/AssemblyInfo.cs
@@ -19,7 +19,7 @@ using System.Security;
 [assembly: AssemblyProductAttribute("SpecExpress.Test.Domain")]
 [assembly: ComVisibleAttribute(false)]
 [assembly: CLSCompliantAttribute(true)]
-[assembly: AssemblyVersionAttribute("1.1.44398")]
+[assembly: AssemblyVersionAttribute("1.1.45068")]
 [assembly: AssemblyDelaySignAttribute(false)]
 [assembly: AssemblyCopyrightAttribute("Copyright, Alan Baker 2009")]
 
--- SpecExpress/src/SpecExpress.Web/Properties/AssemblyInfo.cs
+++ SpecExpress/src/SpecExpress.Web/Properties/AssemblyInfo.cs
@@ -19,7 +19,7 @@ using System.Security;
 [assembly: AssemblyProductAttribute("SpecExpress.Web")]
 [assembly: ComVisibleAttribute(false)]
 [assembly: CLSCompliantAttribute(true)]
-[assembly: AssemblyVersionAttribute("1.1.41649")]
+[assembly: AssemblyVersionAttribute("1.1.45068")]
 [assembly: AssemblyDelaySignAttribute(false)]
 [assembly: AssemblyCopyrightAttribute("Copyright, Alan Baker 2009")]
 
