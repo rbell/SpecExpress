fdb0caa0684a69c41ca219dbc90c4b5103d8ec1c
TechnoAg <alan@technoag.com>
2011-08-03 07:14:19 -0500

    Scratch

--- SpecExpress/src/SpecExpress.Test/Scratch.cs
+++ SpecExpress/src/SpecExpress.Test/Scratch.cs
@@ -1,17 +1,49 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
+using NUnit.Framework;
+using SpecExpress.DSL;
+using SpecExpress.Enums;
 using SpecExpress.Test.Entities;
 using SpecExpress.Test.Polymorphism;
 
 namespace SpecExpress.Test
 {
+  
+
+    [TestFixture]
     public class Scratch
     {
+
+        private string s0 = "not hello";
+        public string PString
+        {
+            get { return s0; }   
+        }
+
+        public bool DoWorkAssert(string s1, string s2)
+        {
+            string str = "hello";
+          
+
+            AssertSpecification.Assert(sp =>
+            {
+                sp.Check(s => s1).Required().EqualTo(PString);
+            });
+
+            return true;
+        }
+
+        [Test]
         public void Do()
         {
-            
+            Assert.Throws<ValidationException>(() =>
+                                                      {
+                                                          DoWorkAssert("hello", string.Empty);
+                                                      });
+
 
         }
     }
