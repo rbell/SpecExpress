f17dcfa4ec2efdf22502a442f1c6e33ca5fb7b39
Alan Baker <alan@technoag.com>
2011-08-18 13:57:50 -0500

    Added test for Specification Assert and Validate

---
+++ SpecExpress/src/SpecExpress.Test/InlineSpecificationTests.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace SpecExpress.Test
+{
+    [TestFixture]
+    public class InlineSpecificationTests
+    {
+        private string s0 = "not hello";
+        public string PString
+        {
+            get { return s0; }
+        }
+
+        [Test]
+        public void Specification_Assert_ThrowsException()
+        {
+            string localString = "hello";
+
+            Assert.Throws<ValidationException>(() =>
+            {
+                Specification.Assert(sp => sp.Check(s => localString).Required().EqualTo(PString));
+            });
+        }
+
+        [Test]
+        public void Specification_Validate_IsInvalid()
+        {
+            var localString = "hello";
+            var vn = Specification.Validate(sp => sp.Check(s => localString).Required().EqualTo(PString));
+
+            Assert.That(vn.IsValid, Is.False);
+        }
+    }
+}
--- SpecExpress/src/SpecExpress.Test/Scratch.cs
+++ SpecExpress/src/SpecExpress.Test/Scratch.cs
@@ -1,53 +1,46 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Linq.Expressions;
-using System.Text;
-using NUnit.Framework;
-using SpecExpress.DSL;
-using SpecExpress.Enums;
-using SpecExpress.Test.Entities;
-using SpecExpress.Test.Polymorphism;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Linq.Expressions;
+//using System.Text;
+//using NUnit.Framework;
+//using SpecExpress.DSL;
+//using SpecExpress.Enums;
+//using SpecExpress.Test.Entities;
+//using SpecExpress.Test.Polymorphism;
 
-namespace SpecExpress.Test
-{
+//namespace SpecExpress.Test
+//{
   
 
-    [TestFixture]
-    public class Scratch
-    {
+//    [TestFixture]
+//    public class Scratch
+//    {
 
-        private string s0 = "not hello";
-        public string PString
-        {
-            get { return s0; }   
-        }
+//        private string s0 = "not hello";
+//        public string PString
+//        {
+//            get { return s0; }   
+//        }
 
-        public bool DoWorkAssert(string samSpade, string s2)
-        {
-            string str = "hello";
-          
+//        [Test]
+//        public void Specification_Assert_ThrowsException()
+//        {
+//            string localString = "hello";
 
-            //Specification.Assert(sp =>
-            //{
-            //    sp.Check(s => s1).Required().EqualTo(PString);
-            //});
+//            Assert.Throws<ValidationException>(() =>
+//                                                      {
+//                                                          Specification.Assert(sp => sp.Check(s => localString).Required().EqualTo(PString));
+//                                                      });
+//        }
 
+//        [Test]
+//        public void Specification_Validate_IsInvalid()
+//        {
+//            var localString = "hello";
+//            var vn = Specification.Validate(sp => sp.Check(s => localString).Required().EqualTo(PString));
 
-            var vn = Specification.Validate(sp => sp.Check(s => samSpade).Required().EqualTo(PString));
-
-            return true;
-        }
-
-        [Test]
-        public void Do()
-        {
-            Assert.Throws<ValidationException>(() =>
-                                                      {
-                                                          DoWorkAssert("hello", string.Empty);
-                                                      });
-
-
-        }
-    }
-}
+//            Assert.That(vn.IsValid, Is.False);
+//        }
+//    }
+//}
