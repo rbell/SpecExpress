9e4d35811f1bf01fb4f53a0e1450a27b3eab7b64
TechnoAg <alan@technoag.com>
2012-01-10 21:11:56 -0600

    Fixed bug with InLine Specification. ForEachSpecifications used in an inline specification that depended on the ValidationCatalog was throwing a NullObject exception.

--- SpecExpress/src/SpecExpress.Test/InlineSpecificationTests.cs
+++ SpecExpress/src/SpecExpress.Test/InlineSpecificationTests.cs
@@ -9,7 +9,27 @@ namespace SpecExpress.Test
     [TestFixture]
     public class InlineSpecificationTests
     {
+
+        public class Person
+        {
+            public string FirstName { get; set; }
+            public string LastName { get; set; }
+        }
+
+
+        public class PersonSpecification : Validates<Person>
+        {
+            public PersonSpecification()
+            {
+                Check(p => p.LastName).Required();
+            } 
+        }
+
+       
         private string s0 = "not hello";
+
+        private List<Person> _persons = new List<Person>();
+
         public string PString
         {
             get { return s0; }
@@ -34,5 +54,23 @@ namespace SpecExpress.Test
 
             Assert.That(vn.IsValid, Is.False);
         }
+
+        [Test]
+        public void Specification_WithForEachSpecification_UsingValidationCatalog_Assert_ThrowsException()
+        {
+            //Add spec to catalog
+            ValidationCatalog.AddSpecification<PersonSpecification>();
+
+            //Setup test data
+            _persons.Add(new Person() {FirstName = "First", LastName = "Last"});
+            _persons.Add(new Person() { FirstName = "First" }); //Missing Last Name
+            
+
+            Assert.Throws<ValidationException>(() =>
+            {
+                Specification.Assert(sp => sp.Check(s => _persons).Required().ForEachSpecification());
+            });
+        }
+
     }
 }
